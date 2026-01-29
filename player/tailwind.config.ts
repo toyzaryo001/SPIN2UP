5bf958c030c8f1fa7a89197bc0f29d458865c5e7
toyzaryo001 <toyzaryo001@gmail.com>
2026-01-29 18:13:45 +0700

    Fix Layout: Add missing tailwind.config.ts and rebuild styles

---
+++ player/tailwind.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+    content: [
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {
+                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+                "gradient-conic":
+                    "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+            },
+            fontFamily: {
+                kanit: ['Kanit', 'sans-serif'],
+            }
+        },
+    },
+    plugins: [],
+};
+export default config;
